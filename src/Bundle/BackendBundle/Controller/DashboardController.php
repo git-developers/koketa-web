3de9486168583e2f82132f80592e9a5a3a472634
Tianos <josetue@outlook.com>
2019-09-07 16:45:23 -0500

    Tianos said -> modifications made on: 2019-09-07 . 16:45:22

--- src/Bundle/BackendBundle/Controller/DashboardController.php
+++ src/Bundle/BackendBundle/Controller/DashboardController.php
@@ -20,43 +20,6 @@ class DashboardController extends BaseController
      */
     public function indexAction(Request $request): Response
     {
-	
-/*	    $user = $this->getUser();
-	
-	    if ($user == null) {
-		    echo 'USER NULL :::: PDV ---- DefaultController<pre>';
-		    exit;
-	    }
-	    
-	    echo 'PDV USER ---- DefaultController<pre>';
-	    print_r($user->getRoles());
-	    exit;*/
-
-
-//	    $token = $this->container->get('security.token_storage')->getToken();
-//	    $user = $token->getUser();
-        
-//	    echo "POLLO:: <pre>";
-//	    print_r($user->getPointOfSaleActive()->getName());
-//	    exit;
-
-
-
-//        echo 'PDV DefaultController --- GATO:::<pre>';
-//        print_r($user->getRoles());
-//        exit;
-
-
-//        $pointOfSales = $this->getUser()->getPointOfSale();
-//
-//        foreach ($pointOfSales as $key => $pointOfSale) {
-//
-//            echo "<pre>";
-//            print_r($pointOfSale->getName());
-//
-//        }
-//
-//        exit;
 
         $options = $request->attributes->get('_tianos');
 
